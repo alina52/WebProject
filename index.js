ce036047b2feb680d97ff3c5a45869d257421c1c
Nee <183540581@qq.com>
2017-06-04 21:41:43 +0800

    a

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var app = express();
-var http = require('http');
-var server = http.createServer(app);
 
 var path = require('path');
-var port = process.env.PORT || 3000;
-server.listen(port);
 var request = require('request');
-var bodyParser = require('body-parser');	
-var https = require('https');
-var fs = require('fs');
+var bodyParser = require('body-parser');
 
 
 // 跨域访问
@@ -17,18 +11,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.all('*',function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , PRIVATE-TOKEN');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  next();
-});
+// app.all('*',function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , PRIVATE-TOKEN, JSON');
+//   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+//   next();
+// });
 
-app.use('/', function (req, res, next) {
-    res.sendFile('public/index.html',  {root: __dirname});
-})
+app.get('/', function(req,res,next){
+    res.sendFile('public/welcome.html', { root: __dirname });
+});
 
 app.use((req,res,next)=>{
     res.send("404 not found");
 })
 
+app.listen(3000);
